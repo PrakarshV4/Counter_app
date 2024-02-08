097df62801cb6dd0af127dcd8116bd9f35c050d3
Prakarsh Verma <prakarsh1248@gmail.com>
2024-02-08 21:48:41 +0530

    Version 1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+//hook 
+import { useState } from  "react"
+
+function App() {
+  //creating a state object
+  const [count,setCount] = useState(0);
+  
+  return (
+    <div>
+      <CustomButton count={count} setCount={setCount} ></CustomButton>
+    </div>
+  )
+}
+//component
+function CustomButton(props){
+
+  function onClickHandler(){
+    props.setCount(props.count + 1);
+  }
+
+  return <button onClick={onClickHandler}>
+    Counter {props.count}
+  </button>
+}
+
+export default App
